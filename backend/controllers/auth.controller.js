5a9ebf472cb3aa6a202d8f4acf38c57ee33d5e04
nirmal-blip <nirmal.joshi.23co@iitram.ac.in>
2026-04-19 11:18:53 +0530

    changed backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -12,8 +12,9 @@ const signToken = (user) =>
 const setCookie = (res, token) =>
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    secure: process.env.NODE_ENV === 'production', // True in production
+    // 'none' is required for cross-site cookies when secure is true
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', 
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
